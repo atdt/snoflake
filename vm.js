8c5f8b90c81aa1af96ce147b11be93306e8cc783
Ori Livneh <ori.livneh@gmail.com>
2012-04-05 19:37:35 -0400

    scaffold of SIL machine

--- vm.js
+++ vm.js
@@ -3,18 +3,29 @@
 // Released under GPL and MIT licenses
 
 (function () {
-    var frame, p = 0, rdata = [];
 
-    function exec(op, operands, dest) {
-        var retval = op.apply(null, args);
+    // << compiled SIL gets inlined here >>
+
+    var end = stack.length,
+        frame = [],
+        p = 0,
+        resident = [];
+
+    // << macro defn's go here >>
+
+    function exec(macro, operands, dest) {
+        var stack_loc = p - 1,
+            func = resident[macro],
+            retval = func.apply(null, operands);
 
         if (dest !== null) {
-            rdata[dest] = retval || p;
+            resident[dest] = (retval !== undefined) ? retval : stack_loc;
         }
     }
 
     do {
-        frame = stack[p];
+        frame = stack[p++];
         exec.apply(null, frame);
-    } while (p < stack.length);
+    } while (p < end)
+
 }());
