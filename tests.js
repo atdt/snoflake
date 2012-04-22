9c5373bd144b8a714cb936d7e4dba9514fdb2c8a
Ori Livneh <ori.livneh@gmail.com>
2012-04-22 03:39:56 -0400

    more tests

--- tests.js
+++ tests.js
@@ -9,9 +9,69 @@ test('string encoding', function () {
 });
 
 test('real numbers', function () {
-    var raw = 1042284544,
+    var uint = 1042284544,
         real = 0.15625;
 
-    strictEqual(asFloat32(raw), real, 'uInt32 -> Float32');
-    strictEqual(asUint32(real), raw, 'Float32 -> uInt32');
+    strictEqual(asFloat32(uint), real, 'uInt32 -> Float32');
+    strictEqual(asUint32(real), uint, 'Float32 -> uInt32');
+});
+
+
+test('uint32 validation', function () {
+    var values = [
+        [ Infinity,          false, ],
+        [ -Infinity,         false, ],
+        [ null,              false, ],
+        [ undefined,         false, ],
+        [ NaN,               false, ],
+        [ 0,                 true,  ],
+        [ 0,                 true,  ],
+        [ 1,                 true,  ],
+        [ -1,                false, ],
+        [ 1.2,               false, ],
+        [ -1.2,              false, ],
+        [ 10000,             true,  ],
+        [ -10000,            false, ],
+        [ 2147483647,        true,  ],
+        [ 2147483648,        true,  ],
+        [ 4294967296,        false, ],
+        [ -4294967296,       false, ],
+        [ 4294967296.123,    false, ]
+    ];
+
+    values.forEach(function (data) {
+        var value = data[0],
+            is_uint32 = data[1],
+            msg = [
+                value,
+                is_uint32 ? 'is' : 'is not',
+                'an unsigned 32-bit integer'
+            ].join(' ');
+
+        strictEqual(isUint32(value), is_uint32, msg);
+    });
+});
+
+module('descriptors');
+
+test('loading descriptors', function () {
+    var created = new Descriptor(),
+        loaded = Descriptor(created.offset);
+
+    created.setAddress(123);
+    strictEqual(
+        created.getAddress(),
+        loaded.getAddress(),
+        'identical descriptors share state'
+    );
+});
+
+
+test('typecasting', function () {
+    var desc = new Descriptor(),
+        uint = 1042284544,
+        real = 0.15625;
+
+    desc.setAddress(uint);
+    strictEqual(desc.getAddressReal(), real);
 });
