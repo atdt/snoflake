e17decfcfcb22c191f3df8e38c78f3aa4fd4e294
Ori Livneh <ori.livneh@gmail.com>
2012-05-19 21:30:08 -0700

    added tests for typed getters/setters

--- test/string-test.js
+++ test/string-test.js
@@ -1,24 +1,62 @@
 /*jslint node: true, white: true, sloppy: true */
 /*global assert */
 
-var buster = require( 'buster' );
-    Snoflake = require( '../lib/snoflake' );
-    str = Snoflake.str,
+var buster = require( 'buster' ),
+    Snoflake = require( '../lib/snoflake' ),
     haya = [ -16777473, 154795775, 154077502 ]; // हाय
 
-buster.testCase( 'Strings', {
+with ( Snoflake ) {
 
-    encode: function () {
-        assert.equals( str.encode('हाय'), haya );
-    },
+    buster.testCase( 'Strings', {
+        encode: function () {
+            assert.equals( str.encode('हाय'), haya );
+        },
+        decode: function () {
+            assert.equals( str.decode(haya), 'हाय' );
+        },
+        invertible: function () {
+            var hi = 'こんにちは';
+            assert.equals( str.decode( str.encode( hi ) ), hi );
+        }
+    } );
 
-    decode: function () {
-        assert.equals( str.decode(haya), 'हाय' );
-    },
+    buster.testCase( 'Typed Setters', {
+        uint: function () {
+            assert.exception( function () {
+                setUint( 0, -4 );
+            }, 'RangeError' );
+        },
+        int: function () {
+            assert.exception( function () {
+                setUint( 0, 4.2 );
+            }, 'RangeError' );
+        },
+        real: function () {
+            assert.exception( function () {
+                setReal( 0, 10e100 );
+            }, 'RangeError' );
+        }
+    } );
 
-    invertible: function () {
-        var hi = 'こんにちは';
-        assert.equals( str.decode( str.encode( hi ) ), hi );
-    }
+    buster.testCase( 'Typed Getters', {
+        uint: function () {
+            setUint( 0, 123 );
+            assert.equals( getUint(0), 123 );
+        },
+        int: function () {
+            setInt( 0, -123 );
+            assert.equals( getInt(0), -123 );
+        },
+        real: function () {
+            setReal( 0, 3.14 );
+            assert.equals( Math.floor( getReal(0) ), 3 );
+        }
+    } );
+}
 
-});
+/*
+    var setUint = Snoflake.setUint,
+        setInt = Snoflake.setInt,
+
+    buster.testCase( 'Typed Descriptors', 
+    */
