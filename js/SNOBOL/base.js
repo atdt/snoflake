fcdf96961f83004226cbf21aa7064c4b11508fd1
Ori Livneh <ori.livneh@gmail.com>
2022-01-26 00:47:59 -0500

    Remove SNOBOL.extend; unused.

--- js/SNOBOL/base.js
+++ js/SNOBOL/base.js
@@ -1,14 +1,6 @@
 "use strict";
 
 var SNOBOL = {
-    extend: function ( dst, src ) {
-        for ( var attr in src ) {
-            if ( src.hasOwnProperty( attr ) ) {
-                dst[ attr ] = src[ attr ];
-            }
-        }
-        return dst;
-    },
     config: { debug: false },
     log: function () {
         if ( SNOBOL.config.debug ) {
